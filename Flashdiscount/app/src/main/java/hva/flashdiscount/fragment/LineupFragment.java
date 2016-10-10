56931c90bb4bfae328027be3c8a7d9dd3233c5d6
Anthony Sitaram <anthony.sitaram@hva.nl>
2016-10-11 00:30:18 +0200

    Added GPS, Only makes API call if location is known

--- Flashdiscount/app/src/main/java/hva/flashdiscount/fragment/LineupFragment.java
+++ Flashdiscount/app/src/main/java/hva/flashdiscount/fragment/LineupFragment.java
@@ -1,8 +1,15 @@
 package hva.flashdiscount.fragment;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +22,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,7 +39,9 @@ import hva.flashdiscount.model.Company;
 import hva.flashdiscount.model.Discount;
 import hva.flashdiscount.model.Establishment;
 
-public class LineupFragment extends Fragment {
+public class LineupFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     View rootView;
     ExpandableListView expandableListView;
@@ -38,36 +51,56 @@ public class LineupFragment extends Fragment {
     private RequestQueue requestQueue;
     private Context context;
     OnListDataListener listDataCallback;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
 
+    public void askLocationPermission(){
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
 
     public LineupFragment() {
 
     }
 
-    public interface OnListDataListener {
-        void onListDataChange();
-    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+                    Log.e("permission", "GRANTED");
+                    mGoogleApiClient.connect();
 
-        context = getActivity();
-        requestQueue = Volley.newRequestQueue(context);
+                } else {
 
+                    Log.e("permission", "DENIED");
+                }
+                return;
+            }
+        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_lineup, container, false);
+    public void onConnected(@Nullable Bundle bundle) {
 
-        return rootView;
-    }
+        if ( Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
 
-    @Override
-    public void onResume(){
-        super.onResume();
-
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Log.e("latitude", String.valueOf(mLastLocation.getLatitude()));
+        Log.e("longitude", String.valueOf(mLastLocation.getLongitude()));
         establishments = new ArrayList<>();
+
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, server_url,
                 new Response.Listener<JSONObject>() {
                     @Override
@@ -99,7 +132,6 @@ public class LineupFragment extends Fragment {
 
                             expandableListView = (ExpandableListView) getActivity().findViewById(R.id.expListView);
                             expandableListView.setAdapter(new ExpandableListAdapter(establishments, getActivity()));
-                            Log.e("establionee", String.valueOf(establishments.size()));
                             expandableListView.setGroupIndicator(null);
 
                             try {
@@ -123,5 +155,55 @@ public class LineupFragment extends Fragment {
 
         );
         requestQueue.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    public interface OnListDataListener {
+        void onListDataChange();
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        context = getActivity();
+        requestQueue = Volley.newRequestQueue(context);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_lineup, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        if ( Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            askLocationPermission();
+        }
+
     }
 }
