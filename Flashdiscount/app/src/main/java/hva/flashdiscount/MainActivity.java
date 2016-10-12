343dd12e241282204f03099dbc1eb7c3eb75d60c
Sean Molenaar <sean@m2mobi.com>
2016-10-12 16:51:02 +0200

    Adding back menu button

--- Flashdiscount/app/src/main/java/hva/flashdiscount/MainActivity.java
+++ Flashdiscount/app/src/main/java/hva/flashdiscount/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -59,6 +60,11 @@ public class MainActivity extends AppCompatActivity
 
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
