ebe5760f7753e248152d176c852ec5c7f5700a83
Ezra <ezra-waalboer@hotmail.com>
2016-10-26 17:42:39 +0200

    Added VolleySingleton imageloader for images

--- Flashdiscount/app/src/main/java/hva/flashdiscount/fragment/DetailFragment.java
+++ Flashdiscount/app/src/main/java/hva/flashdiscount/fragment/DetailFragment.java
@@ -9,9 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.google.gson.Gson;
 
+
 import hva.flashdiscount.R;
+import hva.flashdiscount.Utils.VolleySingleton;
 import hva.flashdiscount.model.Discount;
 import hva.flashdiscount.model.Establishment;
 
@@ -22,15 +26,19 @@ public class DetailFragment extends Fragment {
 
     private static final String TAG = DetailFragment.class.getSimpleName();
 
+    private ImageLoader mImageLoader;
     private View mRootView;
     private Establishment establishment;
     private Discount discount;
+
+    private NetworkImageView companyImage;
 
     private TextView companyName;
     private TextView companyDescription;
     private TextView claimsLeft;
     private TextView timeLeft;
     private TextView discountDescription;
+
 
     public DetailFragment() {
     }
@@ -64,6 +72,7 @@ public class DetailFragment extends Fragment {
 
         if(mRootView == null){
             mRootView = inflater.inflate(R.layout.fragment_discount_detail, container, false);
+            mImageLoader = VolleySingleton.getInstance(getActivity()).getImageLoader();
             initViews();
             setCompanyText();
             setDiscountText();
@@ -79,6 +88,7 @@ public class DetailFragment extends Fragment {
     }
 
     public void initViews(){
+        companyImage = (NetworkImageView) mRootView.findViewById(R.id.company_detail_image);
         companyName = (TextView) mRootView.findViewById(R.id.company_name);
         companyDescription = (TextView) mRootView.findViewById(R.id.company_description);
         claimsLeft = (TextView) mRootView.findViewById(R.id.claims_left);
@@ -87,6 +97,10 @@ public class DetailFragment extends Fragment {
     }
 
     public void setCompanyText(){
+        //String urlString = "http://www.arenapoort.nl/wp-content/uploads/2014/05/12874-proost.jpg";
+        companyImage.setImageUrl(establishment.getCompany().getLogo(), mImageLoader);
+
+
         companyName.setText(establishment.getCompany().getName());
         companyDescription.setText(establishment.getCompany().getDescription());
     }
