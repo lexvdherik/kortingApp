18468682444420b18cd0aa4a4adf3e2ffa497ae0
Maiko1100 <maiko1100@gmail.com>
2016-10-27 14:31:14 +0200

    safadsfsdfasf

--- Flashdiscount/app/src/main/java/hva/flashdiscount/fragment/LoginDialogFragment.java
+++ Flashdiscount/app/src/main/java/hva/flashdiscount/fragment/LoginDialogFragment.java
@@ -9,7 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
@@ -17,9 +19,10 @@ import hva.flashdiscount.R;
 
 public class LoginDialogFragment extends DialogFragment {
 
-
+    private final int RC_SIGN_IN = 1;
     private static final String TAG = LoginDialogFragment.class.getSimpleName();
 
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,20 +36,63 @@ public class LoginDialogFragment extends DialogFragment {
                 .requestEmail()
                 .build();
 
-        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this.getActivity())
+         mGoogleApiClient = new GoogleApiClient.Builder(this.getActivity())
                 //.enableAutoManage(this/* FragmentActivity */, (GoogleApiClient.OnConnectionFailedListener) this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-      //  Log.e(TAG, rootView.findViewById(R.id.title).toString());
+
+
         SignInButton signInButton = (SignInButton) rootView.findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
         signInButton.setScopes(gso.getScopeArray());
 
-        rootView.findViewById(R.id.sign_in_button).setOnClickListener();
+        rootView.findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+
+                }
+            }
+        });
 
 
 
         return rootView;
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+            Log.e(TAG,"Ik chill hem in hier");
+        }
+    }
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            Log.e(TAG,acct.getEmail());
+           // mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+           // updateUI(true);
+        } else {
+
+            // Signed out, show unauthenticated UI.
+           // updateUI(false);
+        }
     }
 
 }
