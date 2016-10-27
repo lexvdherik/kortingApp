1d6d10fcebfdc8f08698ba482c62eb5990f4be04
Maiko1100 <maiko1100@gmail.com>
2016-10-27 13:44:04 +0200

    lk;lk

--- Flashdiscount/app/src/main/java/hva/flashdiscount/fragment/LoginDialogFragment.java
+++ Flashdiscount/app/src/main/java/hva/flashdiscount/fragment/LoginDialogFragment.java
@@ -2,10 +2,14 @@ package hva.flashdiscount.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import hva.flashdiscount.R;
 
@@ -13,9 +17,26 @@ public class LoginDialogFragment extends DialogFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.e("doet", "het?");
         View rootView = inflater.inflate(R.layout.fragment_login_dialog, container, false);
+
         getDialog().setTitle("Login Dialog");
+
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this.getActivity())
+                //.enableAutoManage(this/* FragmentActivity */, (GoogleApiClient.OnConnectionFailedListener) this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton signInButton = (SignInButton) getActivity().findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setScopes(gso.getScopeArray());
+
+
         return rootView;
     }
 }
