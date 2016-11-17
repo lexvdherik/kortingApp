6734d7cfb568404deea76962a9fd4423e29f8039
Maiko1100 <maiko1100@gmail.com>
2016-11-17 15:46:20 +0100

    lk

--- Flashdiscount/app/src/main/java/hva/flashdiscount/Network/CustomRequest.java
+++ Flashdiscount/app/src/main/java/hva/flashdiscount/Network/CustomRequest.java
@@ -1,6 +1,8 @@
 package hva.flashdiscount.Network;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
@@ -10,8 +12,12 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
@@ -22,6 +28,10 @@ import com.google.gson.JsonSyntaxException;
 
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.Map;
 
 import hva.flashdiscount.MainActivity;
@@ -53,43 +63,48 @@ class CustomRequest<T> extends Request<T> {
                 60000,
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        loginExpired();
 
     }
 
-//    private Boolean loginExpired() {
-//        Calendar currentDate = Calendar.getInstance();
-//        currentDate.setTime(Calendar.getInstance().getTime());
-//
-//        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(applicationContext);
-//
-//        Calendar expireDate = Calendar.getInstance();
-//        SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
-//
-//        try{
-//            expireDate.setTime(sdf.parse(sharedPref.getString("expireDate", "")));
-//        } catch(ParseException e) {
-//            Log.e(TAG, e.getMessage());
-//        }
-//
+    private Boolean loginExpired() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(token)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(applicationContext)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        Calendar currentDate = Calendar.getInstance();
+        currentDate.setTime(Calendar.getInstance().getTime());
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+
+        Calendar expireDate = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+
+        try{
+            expireDate.setTime(sdf.parse(sharedPref.getString("expireDate", "")));
+        } catch(ParseException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
 //        if(currentDate.compareTo(expireDate) != -1) {
-//            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                    .requestIdToken(token)
-//                    .requestEmail()
-//                    .build();
-//
-//            mGoogleApiClient = new GoogleApiClient.Builder(applicationContext)
-//                    .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-//                    .build();
-//
-//            acct = result.getSignInAccount();
-//
-//
-//        } else {
-//            this.idToken = sharedPref.getString("idToken", "");
-//        }
-//
-//        return true;
-//    }
+
+            OptionalPendingResult<GoogleSignInResult> pendingResult =
+                    Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+            if (pendingResult.isDone()) {
+                // There's immediate result available.
+                pendingResult.get();
+//            }
+
+
+        } else {
+            this.idToken = sharedPref.getString("idToken", "");
+        }
+
+        return true;
+    }
 
     protected Map<String, String> getParams() throws AuthFailureError {
         return params;
